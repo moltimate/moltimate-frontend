b978890cc81d927540b902e049823e96a4cffbd1
Shannon McIntosh <smcintosh881@gmail.com>
2018-10-05 00:44:41 -0400

    Fix #2

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Menu></Menu>
-        <ProteinContainer></ProteinContainer
+        <Menu />
+        <ProteinContainer />
       </div>
     );
   }
