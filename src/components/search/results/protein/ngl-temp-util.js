a0c246bf4001837cab536600af5c1c309b1b67e4
Josh Miller <jxm7374@rit.edu>
2018-11-19 13:03:50 -0500

    Add alignment

--- src/components/search/results/protein/ngl-temp-util.js
+++ src/components/search/results/protein/ngl-temp-util.js
@@ -15,14 +15,14 @@ export function init(base, compare) {
   }, false );
 
   Promise.all([
-    stage.loadFile(`rcsb://${base}.pdb`).then(function (o) {
+    stage.loadFile(`rcsb://${base}`).then(function (o) {
       var query = compare.alignedResidues.map(i => i.residueId +":"+i.chainName).join(" or ");
-      o.addRepresentation('ball+stick', { sele: query, color: 'lightgreen', colorScheme: 'bfactor' });
+      o.addRepresentation('ball+stick', { sele: query, color: 'lightgreen'});
       o.autoView();
       return o;
     }),
 
-    stage.loadFile(`rcsb://${compare.motifPdbId}.pdb`).then(function (o) {
+    stage.loadFile(`rcsb://${compare.motifPdbId}`).then(function (o) {
       var query = compare.activeSiteResidues.map(i => i.residueId +":"+i.chainName).join(" or ");
       o.addRepresentation('ball+stick', { sele: query, color: 'tomato' });
       o.autoView();
@@ -33,7 +33,7 @@ export function init(base, compare) {
     var s2 = ol[ 1 ].structure;
     var q1 = compare.alignedResidues.map(i => i.residueId +":"+i.chainName).join(" or ");
     var q2 = compare.activeSiteResidue.map(i => i.residueId +":"+i.chainName).join(" or ");
-    NGL.superpose(s1, s2, true, q2, q1);
+    NGL.superpose(s1, s2, true, q1, q2);
     ol[ 0 ].updateRepresentations({ position: true });
     ol[ 0 ].autoView();
   });
