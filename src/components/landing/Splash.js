9886cfd226d65eebb28709748c527e7f51cce644
Shannon McIntosh <smcintosh881@gmail.com>
2018-12-15 00:00:18 -0500

    add protein place holder image

--- src/components/landing/Splash.js
+++ src/components/landing/Splash.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import { init } from '../protein/ngl-util.js';
+import Logo from './logo.png';
+import Protein from './protein.png';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import SvgIcon from '@material-ui/core/SvgIcon';
-import ProteinView from '../protein/ProteinView';
 
 const styles = {
   root: {
@@ -21,6 +21,15 @@ const styles = {
     color: '#434e61',
     fontFamily: 'Nunito, sans-serif',
     display: 'flex',
+  },
+  logo: {
+    height: '60px',
+    padding: '10px',
+  },
+  protein: {
+    marginRight: '9%',
+    height: '600px',
+    marginTop: '3%'
   },
   float: {
     width: '100%',
@@ -39,16 +48,21 @@ const styles = {
     //color: '#2AF598',
     marginRight: '20px',
   },
+  inlineFlex: {
+    display: 'inline-flex',
+    width: '100%',
+  },
   titleContainer: {
-    width: '50%',
-    margin: '20%',
+    marginLeft: '15%',
+    marginTop: '18%'
   },
   title: {
     fontSize: '40px',
   },
   subTitle: {
     fontSize: '20px',
-    fontWeight: '200'
+    fontWeight: '200',
+    width: '70%',
   },
 };
 
@@ -60,6 +74,7 @@ function Splash(props) {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar className={classes.toolBar}>
+          <img className={classes.logo} src={Logo} />
           <div className={classes.float}>
             <Button className={classes.button}>About</Button>
             <Button className={classes.button}>Publications</Button>
@@ -70,12 +85,12 @@ function Splash(props) {
           </div>
         </Toolbar>
       </AppBar>
-      <div className={classes.titleContainer}>
-        <div>
+      <div className={classes.inlineFlex}>
+        <div className={classes.titleContainer}>
           <Typography className={classes.title}>Moltimate Protein Analysis</Typography>
-          <Typography className={classes.subTitle}>Molecular visualization tool using catalytic site homology to predict the function of proteins with unknown functions. </Typography>
+          <Typography className={classes.subTitle}>Molecular visualization using catalytic site homology to predict unknown functions of proteins. </Typography>
         </div>
-        <ProteinView isExample={true}/>
+        <img className={classes.protein} src={Protein} />
       </div>
     </div>
   );
