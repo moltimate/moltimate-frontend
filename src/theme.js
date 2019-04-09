ad3a1ddeba6ef9da412c11a6d8c8e0cee2b34a1e
Josh Miller <jxm7374@rit.edu>
2019-04-09 15:29:45 -0400

    Update app yaml

--- src/theme.js
+++ src/theme.js
@@ -2,16 +2,9 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 export const theme = createMuiTheme({
   overrides: {
-<<<<<<< HEAD
-    MuiButton: { // Name of the component ⚛️ / style sheet
-      primary: { // Name of the rule
-        color: 'green', // Some CSS
-      },
-=======
     palette: {
       primary: { main: "#3f51b5", contrastText: "#0f0" },
       secondary: { main: "#00D3A9", contrastText: "#f00" }
->>>>>>> dev
     },
   },
   typography: {
