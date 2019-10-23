cf687d1413a2c155b234e472280b2c27b0f91687
Eric Tiano <etiano417@gmail.com>
2019-10-23 14:40:10 -0400

    Added landing menu cards for docking
     - Added ImportedLigandsContainer
        - Added menu card with title and icon (no content)
     - Added LigandLibraryContainer
        - Added menu card with title and icon (no content)
     - Added new containers to MoltimateContainer

--- src/MoltimateContainer.js
+++ src/MoltimateContainer.js
@@ -2,23 +2,12 @@ import React, { useState, useEffect } from "react";
 import PropTypes from 'prop-types';
 import axios from "axios";
 
-import {library_ligands,
-  test_ligands,
-  fake_docking_data,
-  fake_docking_data_2,
-  test_sites} from './DummyData'
-
-import TopBar from './TopBar';
-import Button from '@material-ui/core/Button'
-
 import SearchContainer from './search/SearchContainer';
 import BuilderContainer from './builder/BuilderContainer';
 import ProteinContainer from './protein/ProteinContainer';
-import DockingContainer from './protein/DockingContainer';
 import LigandLibraryContainer from './ligand_library/LigandLibraryContainer';
 import ImportedLigandsContainer from './imported_ligands/ImportedLigandsContainer';
-import DockingInfoContainer from './docking_info/DockingInfoContainer';
-import SettingsContainer from './settings/SettingsContainer';
+import TopBar from './TopBar';
 
 import styles from './styles.js';
 import { withStyles } from '@material-ui/core/styles';
@@ -28,118 +17,15 @@ function MoltimateContainer(props) {
   const [expanded, setExpanded] = useState(false);
   const [ selectedResult, setSelectedResult ] = useState(null);
   const [ nglData, setNglData ] = useState(null);
-  const [ dockingData, setDockingData ] = useState(null);
-
-  //data on all the ligands the user has uploaded
-  const [ uploadedLigands, setUploadedLigands ] = useState(test_ligands);
-  //autopopulating data on ligands
-  const [ libraryLigands, setLibraryLigands ] = useState(library_ligands);
-  //Active site data for 1a0j
-  const [ testSites, setTestSites ] = useState(test_sites);
-  //ligands selected for docking
-  const [selectedLigands, setSelectedLigands] = useState(new Set());
-  //ligands which have been docked
-  const [dockedLigands, setDockedLigands] = useState(new Set());
-  //the ligand selected to be viewed
-  const [viewingLigand, setViewingLigand] = useState(null);
-  //the docking configurations available for the viewing ligand
-  const [dockingConfigs, setDockingConfigs] = useState([]);
-  //the docking configuration selected to be viewed
-  const [selectedDockingConfig, setSelectedDockingConfig] = useState(null);
-  //whether the settings are showing or now
-  const [showSettings, setShowSettings] = useState(false)
 
   function handleSelectedResult(e, parentId, childId, active, aligned) {
     setSelectedResult({ parentId, childId, active, aligned });
     setNglData({ parentId, childId, active, aligned });
   }
 
-  //Used to toggle the selection of different ligands for docking and viewing
-  function handleSelectedLigand(selected_ligand){
-
-    //if the ligand is already selected for viewing, deselect it
-    if(viewingLigand == selected_ligand){
-      setViewingLigand(null)
-
-    //if docking has already been performed on the selected ligand, select it for viewing
-    }else if(dockedLigands.has(selected_ligand)){
-      setViewingLigand(selected_ligand)
-
-      //this is temporary, for demonstration purposes
-      if(selected_ligand.name == "00I"){
-        setDockingConfigs(fake_docking_data)
-      } else if(selected_ligand.name == "00K") {
-        if(dockingData) {
-            setDockingConfigs(dockingData.data)
-        } else {
-            setDockingConfigs(fake_docking_data_2)
-        }
-      } else {
-        setDockingConfigs(fake_docking_data_2)
-      }
-    }
-
-    //create a copy of the selectedLigands set for editing
-    var new_selected_ligands = new Set(selectedLigands)
-
-    //if the ligand is in already selected for docking, deselect the ligand
-    if (new_selected_ligands.has(selected_ligand)){
-      new_selected_ligands.delete(selected_ligand)
-
-    //if the ligand is not selected for docking, select the ligand
-    } else if(!dockedLigands.has(selected_ligand)){
-      new_selected_ligands.add(selected_ligand)
-    }
-
-    setSelectedLigands(new_selected_ligands)
-  }
-
-  function dockLigands(){
-    var new_docked_ligands =  new Set(selectedLigands)
-    for(let ligand of dockedLigands){
-      new_docked_ligands.add(ligand)
-    }
-    setDockedLigands(new_docked_ligands)
-
-    //if any new ligands were docked, select the first one for display
-    if(selectedLigands.length > 0){
-
-    }
-
-    setSelectedLigands(new Set())
-  }
-
-  /*
-  if the settings menu is showing, close the settings menu
-  if the settings menu is hidden, show the settings menu
-  */
-  function toggleSettingsMenu(){
-    if(showSettings) setShowSettings(false)
-    else setShowSettings(true)
-  }
-
-  function uploadPDBQT( files ) {
-    setDockingData({
-        file: files[0],
-        active_sites: test_sites,
-        ligand_model: 1
-    });
-  }
-
-  function parseBinding( file ) {
-    // Perform parsing here.
-    return fake_docking_data_2;
-  }
-
-  function selectConfig(configSelection){
-    setSelectedDockingConfig(configSelection)
-    console.log("selected config: " + configSelection[1])
-  }
-
     return (
       <>
-        <TopBar toggleSettings = {toggleSettingsMenu} uploadPDBQT = {uploadPDBQT}/>
-
+        <TopBar />
         <div className={classes.controlPanel}>
           <SearchContainer
             handleSelectedResult={handleSelectedResult}
@@ -149,31 +35,8 @@ function MoltimateContainer(props) {
             handleSelectedResult={handleSelectedResult}
             selectedResult={selectedResult}
           />
-          <LigandLibraryContainer
-            library = {libraryLigands}
-            selectedLigands = {selectedLigands}
-            clickLigandHandler = {handleSelectedLigand}
-            dockHandler = {dockLigands}
-            viewingLigand = {viewingLigand}
-            dockedLigands = {dockedLigands}
-          />
-          <ImportedLigandsContainer
-            importedLigands = {uploadedLigands}
-            selectedLigands = {selectedLigands}
-            clickLigandHandler = {handleSelectedLigand}
-            dockHandler = {dockLigands}
-            viewingLigand = {viewingLigand}
-            dockedLigands = {dockedLigands}
-          />
-          {
-            //Only display docking info if there is a viewing ligand selected
-            viewingLigand ? <DockingInfoContainer
-              dockingConfigurations = {dockingConfigs}
-              selectedDockingConfiguration = {selectedDockingConfig}
-              selectConfigurationHandler = {selectConfig}
-            />:null
-          }
-
+          <LigandLibraryContainer/>
+          <ImportedLigandsContainer/>
         </div>
         {
           nglData ? <ProteinContainer
@@ -182,17 +45,6 @@ function MoltimateContainer(props) {
             active={nglData.active}
             aligned={nglData.aligned}
           /> : null
-        }
-        {
-          dockingData ? <DockingContainer
-            file={dockingData.file}
-            ligand_model={dockingData.ligand_model}
-            active_sites={dockingData.active_sites}
-          /> : null
-        }
-        {
-          //Only display the settings modal when showSettings is true
-          showSettings ? <SettingsContainer setShowSettings = {setShowSettings}/>:null
         }
       </>
     );
--- src/ligand_library/LigandLibraryContainer.js
+++ src/ligand_library/LigandLibraryContainer.js
@@ -1,11 +1,11 @@
 import React, {useState} from "react"
-import FilteredLigandResults from '../common/FilteredLigandResults';
+import ResultsBox from '../common/ResultsBox';
 import MenuCard from '../common/MenuCard';
 
 import ListIcon from '@material-ui/icons/List';
 
 function LigandLibraryContainer(props){
-    const {library, selectedLigands, dockedLigands, viewingLigand, clickLigandHandler, dockHandler} = props
+
     const [expandLigandLibrary, setExpandLigandLibrary] = useState(false);
 
     return(
@@ -13,14 +13,7 @@ function LigandLibraryContainer(props){
         label='Ligand Library'
         expand={expandLigandLibrary}
         handleClick={setExpandLigandLibrary}
-        cardChild={<FilteredLigandResults 
-            temp={library} 
-            selectedLigands = {selectedLigands}
-            dockedLigands = {dockedLigands}
-            clickLigandHandler={clickLigandHandler}
-            dockHandler = {dockHandler}
-            viewingLigand = {viewingLigand}
-          />}
+        cardChild={<ResultsBox values={null} handleChange={null} />}
         childIcon={<ListIcon />}
       />
     );
