8c038df5f495d6c5fbbfff6d847ad2c8f528d382
Eric Tiano <etiano417@gmail.com>
2019-10-27 22:10:36 -0400

    List imported ligand names (hardcoded)
     - unformatted list of ligands

--- src/common/LigandResultsBox.js
+++ src/common/LigandResultsBox.js
@@ -1,30 +1,26 @@
 import React, {useState} from 'react';
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { List, ListItem, ListItemText } from '@material-ui/core';
-import LigandResultItem from './LigandResultItem';
 
 export default function LigandResultsBox(props) {
-  const {ligandResults, selectedLigands, dockedLigands, viewingLigand, clickLigandHandler} = props;
-  const box_list = ligandResults.map(current_ligand => <LigandResultItem ligand={current_ligand}/>);
+  const {ligandResults} = props;
 
+  const box_list = ligandResults.map(ligand => <ListItemText>{ligand.name}</ListItemText>);
+    
   return(
+    /*
     <List>
-      {ligandResults.map( ligand => (
-        <LigandResultItem
-          key={ligand.name}
-          ligand={ligand} 
-          isSelected = {selectedLigands.has(ligand)} 
-          isDocked = {dockedLigands.has(ligand)}
-          clickHandler={clickLigandHandler} 
-          isViewingLigand = {ligand == viewingLigand}
-        />))
+      {
+        box_list
       }
     </List>
+    */
+
+    box_list
   );
 }
 
-LigandResultsBox.propTypes = {
-  ligandResults: propTypes.array,
-  selectedLigands: propTypes.instanceOf(Set)
+LigandResultsBox.PropTypes = {
+  ligandResults: PropTypes.array
 };
 
