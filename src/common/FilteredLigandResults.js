97612a128dccc6b07d6a4e31959f78a053ecf3aa
Eric Tiano <etiano417@gmail.com>
2020-01-21 10:21:53 -0500

    Added comments and propTypes to component
     - Added comments and propTypes to FilteredLigandResults

--- src/common/FilteredLigandResults.js
+++ src/common/FilteredLigandResults.js
@@ -11,9 +11,13 @@ import Button from "@material-ui/core/Button";
 import {withStyles} from "@material-ui/core/styles"
 import styles from "./styles.js";
 
-
+/**
+ * A Ligand list with a scroll bar, search box, a "Dock" button to begin a docking
+ * process, and (optionally) an "upload" button to upload ligands
+ */
 function FilteredLigandResults(props) {
-  const { classes, temp, selectedLigands, dockedLigands, clickLigandHandler, dockHandler, viewingLigand } = props;
+  const { classes, temp, selectedLigands, dockedLigands, 
+    clickLigandHandler, dockHandler, viewingLigand } = props;
   const [] = useState();
   return(
     <div>
@@ -43,20 +47,40 @@ function FilteredLigandResults(props) {
         viewingLigand = {viewingLigand}
       />
     </div>
-
   );
 };
-
 FilteredLigandResults.propTypes = {
   classes: propTypes.object,
+  /** 
+   * An array of objects representing ligands available for docking operations.
+   * Example of object inside array: 
+   *   {name:"00I",structure:"C30 H35 N5 O6 S"}  
+   */
+  temp: propTypes.array,
+  /** 
+   * A Set of objects representing ligands selected for docking operations.
+   * Example of object inside the Set: 
+   *   {name:"00I",structure:"C30 H35 N5 O6 S"}  
+   */
+  selectedLigands: propTypes.object,
+  /** 
+   * A Set of objects representing ligands which have had docking operations
+   * performed on them.
+   * Example of object inside the Set: 
+   *   {name:"00I",structure:"C30 H35 N5 O6 S", min_affinity: -5.2}  
+   */
+  dockedLigands: propTypes.object,
+  /** A function that is triggered when a user clicks on a Ligand in the list. */
+  clickLigandHandler: propTypes.func,
+  /** A function that is triggered when a user clicks the "dock" button */
+  dockHandler: propTypes.func,
+  /** The handler for selecting a  */
   handleSelectedResult: propTypes.func,
-  successResult: propTypes.array,
-  failedResult: propTypes.array,
-};
-
-FilteredLigandResults.defaultProps = {
-  successResult: [],
-  
+  /** An object representing a ligand selected to be viewed 
+   *  Example of object:
+   *    {name:"00I",structure:"C30 H35 N5 O6 S", min_affinity: -5.2}  
+  */
+  viewingLigand: propTypes.object,
 };
 
 export default withStyles(styles)(FilteredLigandResults);
