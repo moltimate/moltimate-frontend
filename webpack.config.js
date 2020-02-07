b4fb826f0011b18e26c91a373cc9b42251daf235
Ada Phillips <ada_phillips@mail.rit.edu>
2020-02-07 13:33:53 -0500

    Added favicon to front-end packaging

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const ArchivePlugin = require("webpack-archive-plugin");
+require("file-loader");
 
 module.exports = {
   module: {
@@ -24,13 +25,17 @@ module.exports = {
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.jpe?g$|\.ico$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
+        loader: 'file-loader?name=[name].[ext]'  // <-- retain original file name
       }
     ]
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
-      filename: "./index.html"
+      filename: "index.html"
     }),
     new MiniCssExtractPlugin({
       filename: "[name].css",
