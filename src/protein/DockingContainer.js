0c5d0477072efbceb0753f6193b594a239834617
kdm5498 <kdm5498@rit.edu>
2020-03-03 03:57:04 -0500

    Created display method to display uploaded pdbqt file.

--- src/protein/DockingContainer.js
+++ src/protein/DockingContainer.js
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { loadDocked } from './ngl-util';
+import { loadPDBQT } from './ngl-util';
 
 export default function ProteinContainer(props) {
-  const { file, ligand_model, active_sites } = props;
+  const { file } = props;
   const key = `${file.name}`;
 
   useEffect(() => {
-    loadDocked(file, ligand_model, active_sites);
+    loadPDBQT(file);
   });
 
   return (
@@ -17,6 +17,4 @@ export default function ProteinContainer(props) {
 
 ProteinContainer.propTypes = {
   file: PropTypes.instanceOf(File),
-  ligand_model: PropTypes.number,
-  active_sites: PropTypes.array
 };
