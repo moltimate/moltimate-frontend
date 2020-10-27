a38b01202574bb1c4f9a4d809a4236a70f3b3f21
mev5063 <mev5063@g.rit.edu>
2020-10-27 19:33:04 -0400

    Reorganizing styles (alphabetical)

--- src/common/styles.js
+++ src/common/styles.js
@@ -1,46 +1,32 @@
 export default {
-  search: {
-    margin: '10px 0',
+  builderContainer: {
+    margin: '5% 10%'
   },
-  scrolling: {
-    width: '375px',
-    maxHeight: '350px',
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    margin: '10px 0',
     overflow: 'scroll'
+  },
+  buttonTooltip: {
+    marginTop: '40px',
+    marginLeft: '10px',
+    paddingRight: '5px',
+    color: '#A0A0A0'
+  },
+  cancelButton: {
+    borderRadius: 25,
+    color: 'rgba(0, 0, 0, 0.54)',
+    padding: '10px 20px',
   },
   cyanBackground: {
     background: '#2AF598',
-  },
-  white: {
-    color: 'white',
-  },
-  narrowInput: {
-    width: '75px',
-    margin: '10px'
-  },
-  rounded: {
-    borderRadius: 25,
-    background: '#5433FF',
-    color: 'white',
-    padding: '10px 20px',
   },
   dockButton:{
     float: 'right',
     borderRadius: 25,
     padding: '10px 20px',
     color: 'white',
-  },
-  uploadButton:{
-    float: 'right',
-    borderRadius: 25,
-    padding: '10px 20px',
-    color: 'white',
-  },
-  unselected: {
-    cursor: "pointer",
-  },
-  selected: {
-    background: '#856eff',
-    cursor: "pointer",
   },
   dockedSelected: {
     background: '#e6e6e6',
@@ -49,47 +35,76 @@ export default {
   dockedUnselected: {
     cursor: "pointer",
   },
-  midDocking: {
-    color: '#f2f2f2',
-    opacity: 0.5,
+  filter: {
+    margin: '0% 10%'
   },
-  cancelButton: {
-    borderRadius: 25,
-    color: 'rgba(0, 0, 0, 0.54)',
-    padding: '10px 20px',
+  flexBox: {
+    display: 'flex'
   },
   floatButton: {
     float: 'right',
     marginBottom: '20px',
   },
-  helpIcon: {
-    color: "#404040"
-  },
-  spaced: {
-    margin: '20px 0'
-  },
-  buttonContainer: {
-    display: 'flex',
-    justifyContent: 'center',
-    margin: '10px 0',
-    overflow: 'scroll'
-  },
-  flexBox: {
-    display: 'flex'
-  },
   grey: {
     color: 'rgba(0, 0, 0, 0.54)'
   },
-  padded: {
-    margin: '20px'
+  helpIcon: {
+    color: "#404040"
   },
-  builderContainer: {
-    margin: '5% 10%'
-  },
-  filter: {
-    margin: '0% 10%'
+  helpText : {
+    fontSize: '18px'
   },
   input: {
     display: 'none',
   },
+  labelTooltip: {
+    marginTop: '25px',
+    marginRight: '10px',
+    paddingRight: '5px',
+    color: '#A0A0A0'
+  },
+  midDocking: {
+    color: '#f2f2f2',
+    opacity: 0.5,
+  },
+  narrowInput: {
+    width: '75px',
+    margin: '10px'
+  },
+  padded: {
+    margin: '20px'
+  },
+  rounded: {
+    borderRadius: 25,
+    background: '#5433FF',
+    color: 'white',
+    padding: '10px 20px',
+  },
+  scrolling: {
+    width: '375px',
+    maxHeight: '350px',
+    overflow: 'scroll'
+  },
+  search: {
+    margin: '10px 0',
+  },
+  selected: {
+    background: '#856eff',
+    cursor: "pointer",
+  },
+  spaced: {
+    margin: '20px 0'
+  },
+  unselected: {
+    cursor: "pointer",
+  },
+  uploadButton:{
+    float: 'right',
+    borderRadius: 25,
+    padding: '10px 20px',
+    color: 'white',
+  },
+  white: {
+    color: 'white',
+  }
 }
